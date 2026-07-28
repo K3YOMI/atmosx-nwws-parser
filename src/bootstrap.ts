7bbf5b3a8b0c90387315c95ae688418b91ef0e0a
k3yomi <54733885+K3YOMI@users.noreply.github.com>
2026-07-28 18:10:00 -0500

    update(version): 3.0.621

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -21,7 +21,7 @@ import path from 'path'
 import { EventEmitter } from 'node:events';
 
 export const bootstrap = {
-    version: `3.0.61`,
+    version: `3.0.621`,
     isReady: true,
     ratelimits: {},
     session_xmpp: null,
